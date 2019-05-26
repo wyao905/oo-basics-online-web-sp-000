3252ebcd99af32cd4a3682f4d7ebfdeecf8249d2
Warren Yao <warrenyca905@gmail.com>
2019-05-26 03:57:56 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -19,13 +19,13 @@ class Book
     @author
   end
   
-  def page_count=(num)
-    @page_count = num
-  end
+  # def page_count=(num)
+  #   @page_count = num
+  # end
   
-  def page_count
-    @page_count
-  end
+  # def page_count
+  #   @page_count
+  # end
   
   attr_accessor :genre, :page_count
 end
