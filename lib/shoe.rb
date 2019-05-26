56403a26058c993b5fdd79c84d65f721cf968b11
Warren Yao <warrenyca905@gmail.com>
2019-05-26 04:12:55 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,9 @@
 class Shoe
   def initialize(brand)
     @brand = brand
-    @color = color
-    @size = size
-    @material = material
   end
   
+  attr_writer :color, :size, :material
   attr_reader :brand, :color, :size, :material
   attr_accessor :condition
 end
