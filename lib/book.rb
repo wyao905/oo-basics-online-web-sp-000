1a627c0f143030ec35c721a2fd15e2e6ce527ce7
Warren Yao <warrenyca905@gmail.com>
2019-05-26 03:53:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,7 +3,19 @@ class Book
     @title = title
   end
   
+  def title=(title)
+    @title = title
+  end
+  
   def title
     @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
   end
 end
