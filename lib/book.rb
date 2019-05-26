aea6a45def852cdc2703f984b69f627e1b0d5464
Warren Yao <warrenyca905@gmail.com>
2019-05-26 04:05:39 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,7 +3,8 @@ class Book
     @title = title
   end
   
-  attr_accessor :title, :author, :page_count, :genre
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
